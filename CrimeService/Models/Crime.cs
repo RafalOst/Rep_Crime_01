e0b17cd76384b983ea4086d66d9a6e407767bcfe
Rafal Ostachowicz <rafal.ostachowicz@gmail.com>
2022-02-21 12:01:48 +0100

    Add Crime model

---
+++ CrimeService/Models/Crime.cs
@@ -0,0 +1,38 @@
+﻿using CommonItems;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace CrimeService.Models
+{
+    public class Crime
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Crime")]
+        [JsonPropertyName("Crime")]
+        public CrimeEventType CrimeType { get; set; }
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
+
+        public CrimeReportStatus CrimeReportStatus { get; set; }
+
+        [MinLength(10)]
+        [MaxLength(250)]
+        public string PlaceOfEvent { get; set; }
+
+        public int? AssignedLawEnforcmentId { get; set; }
+
+        [MaxLength(150)]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy HH:mm}")]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
