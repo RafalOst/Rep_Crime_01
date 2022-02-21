f4e22ada3e9ef4fa40beddf02cd9c40e784a35dd
Rafal Ostachowicz <rafal.ostachowicz@gmail.com>
2022-02-21 19:38:25 +0100

    Add models in PoliceServices

--- PoliceService/Models/Crime.cs
+++ PoliceService/Models/Crime.cs
@@ -1,35 +1,27 @@
 ﻿using CommonItems;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace CrimeService.Models
 {
     public class Crime
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id { get; set; }
+        public string Id { get; set; }
 
         public CrimeEventType CrimeType { get; set; }
 
-        [MaxLength(500)]
         public string? Description { get; set; }
 
         public CrimeReportStatus CrimeReportStatus { get; set; }
 
-        [MinLength(10)]
-        [MaxLength(250)]
         public string PlaceOfEvent { get; set; }
 
         public int? AssignedLawEnforcmentId { get; set; }
 
-        [MaxLength(150)]
-        [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy HH:mm}")]
-        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime CreatedDate { get; set; }
     }
 }
---
+++ PoliceService/Models/PoliceOfficer.cs
@@ -0,0 +1,22 @@
+﻿using CrimeService.Models;
+
+namespace PoliceService.Models
+{
+    public class PoliceOfficer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public PoliceRank Rank { get; set; }
+
+        public List<Crime> Crimes { get; set; } = new List<Crime>();
+    }
+
+    public enum PoliceRank
+    {
+        Constable,
+        Inspector,
+        Detective
+    }
+}
